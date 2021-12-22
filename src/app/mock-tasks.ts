260be8ff21cdaa513c0a24689732159c34a499fa
Maxi <maximilianovazquez88@gmail.com>
2021-12-22 18:06:21 -0300

    Agregue mosk de tareas y la inteface de las mismas

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import {Task} From "./Task"
+
+export const TASK: Task[] = [
+    {
+        id: 1,
+        text: "Terminar primer modulo de Angular",
+        day: 'Agosto 5 a las 19:00',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Hacer las compras para la cena",
+        day: 'Agosto 5 a las 17:00',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Investigar sobre Bootstrap",
+        day: 'Agosto 5 a las 19:00',
+        reminder: false
+    },  
+    {
+        id: 4,
+        text: "Leer mi libro favorito",
+        day: 'Agosto 5 a las 21:00',
+        reminder: false
+    },
+]
